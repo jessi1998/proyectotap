96e1a673415d8272058c4ae7d52318b597bd200c
pasante12 <pasante12@gerardoortiz.com>
2020-09-14 23:09:06 -0500

    Tendencias de programacion

---
+++ src/main/java/com/example/proyectoM5A/ProyectoM5AApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyectoM5A;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoM5AApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoM5AApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/proyectoM5A/ProyectoM5AApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyectoM5A;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoM5AApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
